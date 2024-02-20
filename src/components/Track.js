28881d235829d7a3e3b18da7eb33a35338994784
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-20 13:26:48 +0530

    update track.js UI

--- src/components/Track.js
+++ src/components/Track.js
@@ -26,9 +26,30 @@ const Track = () => {
   };
 
   return (
-    <div>
+    <div className='bg-gray-500 h-screen'>
+      <h1 className='flex justify-center text-4xl pt-10'>Hello! 👋</h1>
+      
+      <div className='flex pt-16 items-center justify-evenly px-36'>
+        <div className='bg-red-100 h-60 w-60'>
+            prev
+        </div>
+        <div className='bg-red-500 h-80 w-80 gap-5'>
+            curr
+        </div>
+        <div className='bg-red-800 h-60 w-60'>
+            next
+        </div>
+      </div>
+
       <h2>{songsData[currentSongIndex].song_name}</h2>
       <p>{songsData[currentSongIndex].artist_name}</p>
+
+      <h2>{songsData[Math.abs(currentSongIndex - 1) % songsData.length].song_name}</h2>
+      <p>{songsData[Math.abs(currentSongIndex - 1 - songsData.length) % songsData.length].artist_name}</p>
+
+      <h2>{songsData[(currentSongIndex + 1) % songsData.length].song_name}</h2>
+      <p>{songsData[(currentSongIndex + 1) % songsData.length].artist_name}</p>
+      
       <audio
         src={songsData[currentSongIndex].song_url}
         autoPlay={isPlaying}
