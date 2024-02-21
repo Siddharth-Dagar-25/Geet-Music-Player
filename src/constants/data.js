397ac02ffd0443539045338982eb1ee462bf789f
Anukrati Mehta <anukratimehta@gmail.com>
2024-02-22 02:33:34 +0530

    added 30 songs to the data

--- src/constants/data.js
+++ src/constants/data.js
@@ -53,4 +53,265 @@ export const songsData = [
         song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521986/Arjan_Vailly_-_Aimal_uljzbe.mp3",
         cover_image_url: ""
     },
-]
+    {
+        song_name: "Hua Main",
+        song_length: "4:37",
+        artist_name: ["Raghav Chaitanya", "Manoj Muntashir", "Pritam"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521985/Hua_Main_-_Animal_t3v3ml.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Satranga",
+        song_length: "4:31",
+        artist_name: ["Arijit Singh", "Shreyas Puranik", "Siddharth - Garima"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521988/Satranga_-_Animal_kydtkb.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Papa Meri Jaan",
+        song_length: "5:21",
+        artist_name: ["Sonu NIgam", "Harshavardhan Rameshwar", "Raj Shekhar"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521987/Papa_Meri_Jaan_-_Animal_u6gqb0.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Pehle Bhi Main",
+        song_length: "4:10",
+        artist_name: ["Vishal Mishra", "Raj Shekhar"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521988/Pehle_Bhi_Main_-_Animal_iwaj4e.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Kashmir",
+        song_length: "3:36",
+        artist_name: ["Manan Bharadwaj", "Shreya Ghoshal"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521985/Kashmir_-_Animal_mgco0s.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Saari Duniya Jalaa Denge",
+        song_length: "3:02",
+        artist_name: ["Jaani", "B Praak"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521989/Saari_Duniya_Jalaa_Denge_-_Animal_ru7ckg.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Haiwaan",
+        song_length: "2:42",
+        artist_name: ["Ashim Kemson"],
+        movie_name: "Animal",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708521986/Haiwaan_-_Animal_gqounj.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Ve haaniyaan",
+        song_length: "4:00",
+        artist_name: ["Danny", "Avvy Sra", "Sagar"],
+        movie_name: "",
+        genre: "Love, Romantic",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708520793/Geet/Songs/VeHanniya.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Tere hawale",
+        song_length: "5:46",
+        artist_name: ["Pritam", "Arijit Singh", "Shilpa Rao"],
+        movie_name: "",
+        genre: "Love, Romantic",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708520792/Geet/Songs/TereHawale.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Rang lageya",
+        song_length: "3:47",
+        artist_name: ["Mohit Chauhan", "Rochak Kohli"],
+        movie_name: "",
+        genre: "Love, Romantic",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708520793/Geet/Songs/rang-lgeya.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Oonchi Oonchi Deewarein",
+        song_length: "4:11",
+        artist_name: ["Manan Bhardwaj", "Arijit Singh"],
+        movie_name: "",
+        genre: "Love, Romantic",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708520792/Geet/Songs/UcheeinUcheeinDewwaro.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Khol De Par",
+        song_length: "3:13",
+        artist_name: ["Jasleen Royal", "Arijit Singh", "Raj Shekhar"],
+        movie_name: "",
+        genre: "Love, Romantic",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708520792/Geet/Songs/KholDePar.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "One Love",
+        song_length: "2:39",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534521/One_Love_qc8lil.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Cheques",
+        song_length: "3:03",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534514/Cheques_gejuep.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "King Shit",
+        song_length: "3:46",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534519/King_Shit_pnghrm.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Baller",
+        song_length: "2:28",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534442/Baller_dimgiv.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "No Love",
+        song_length: "2:50",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534520/No_Love_jmvtoc.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Still Rollin",
+        song_length: "2:54",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534523/Still_Rollin_yjqsjo.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "You and Me",
+        song_length: "2:52",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534526/You_and_Me_tnlh2e.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Her",
+        song_length: "2:34",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534516/Her_ela2jz.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Elevated",
+        song_length: "3:20",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534516/Elevated_vurasw.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Dior",
+        song_length: "2:19",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534516/Dior_rzxpk3.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Safety Off",
+        song_length: "2:23",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534522/Safety_Off_opsrlm.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "We Rollin",
+        song_length: "3:19",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534524/We_Rollin_gmopnz.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Offshore",
+        song_length: "2:52",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534520/Offshore_nipxiv.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "OG",
+        song_length: "3:17",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534521/OG_sirlgg.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Hood Anthem",
+        song_length: "3:12",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534517/Hood_Anthem_f5bvpq.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Ice",
+        song_length: "2:54",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534518/Ice_cpwiez.mp3",
+        cover_image_url: ""
+    },
+    {
+        song_name: "Ruthless",
+        song_length: "3:24",
+        artist_name: "",
+        movie_name: "Shubh",
+        genre: "",
+        song_url: "https://res.cloudinary.com/daqtelfvl/video/upload/v1708534522/Ruthless_lxae5o.mp3",
+        cover_image_url: ""
+    },
+];
