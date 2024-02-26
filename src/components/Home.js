57967f31363696daae0f47784c0351e43defc9cf
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-26 11:54:38 +0530

    update home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <>
-            <div className='pt-10 pl-10'>Click the link below</div>
-            <div className='pt-10 pl-10'><Link to="/track" className='text-3xl font-bold underline'>Track</Link></div>
-        </>
+        <div className='flex flex-col items-center justify-center w-full min-h-screen p-4 bg-gray-800 text-white'>
+            <div className='pt-10 pl-10 text-xl text-center lg:text-center'>Gear up for a plunge into uncharted territory, where wonders await or realities shift.</div>
+            <div className='pt-10 pl-10'><Link to="/track" className='text-2xl font-bold underline'>Listen Now</Link></div>
+        </div>
     )
 }
 
