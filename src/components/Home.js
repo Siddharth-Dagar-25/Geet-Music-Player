3e9a69ddd146f822168f7b23dee4cf6c895d157b
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-26 12:00:29 +0530

    update home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ import TotalSongsScript from './TotalSongsScript.js'
 const Home = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full min-h-screen p-4 bg-gray-800 text-white'>
-            <div className='pt-10 pl-10 text-xl text-center lg:text-center'>Gear up for a plunge into uncharted territory, where wonders await or realities shift.</div>
-            <div className='pt-10 pl-10'><Link to="/track" className='text-2xl font-bold underline'>Listen Now</Link></div>
+            <div className='pt-10 text-xl justify-center text-center lg:text-center justify-center'>Gear up for a plunge into uncharted territory, where wonders await or realities shift.</div>
+            <div className='pt-10 justify-center'><Link to="/track" className='text-2xl font-bold underline'>Listen Now</Link></div>
             <div className='absolute bottom-8 left-0 p-4'>
                 <TotalSongsScript />
             </div>
