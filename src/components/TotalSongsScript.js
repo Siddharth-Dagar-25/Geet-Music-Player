79052f620503dbc9f94f108171e3cc138092428a
Siddharth Dagar <sid.dagar0101@gmail.com>
2024-02-27 18:52:46 +0530

    update script.js

--- src/components/TotalSongsScript.js
+++ src/components/TotalSongsScript.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { songsData } from '../constants/data.js';
 
 const TotalSongsScript = () => {
-    const skills = [
+    const skills = useMemo(() => [
         `Geet now boasts a vibrant collection of ${songsData.length} songs.`,
         "Discover diverse tunes for every mood."
-    ];
+    ], []); // Dependency array is empty, indicating it only runs once on mount
+
     const [currentSkill, setCurrentSkill] = useState('');
     const [index, setIndex] = useState(0); // Current skill index
     const [subIndex, setSubIndex] = useState(0); // Index of the current character in the skill string
@@ -13,30 +14,26 @@ const TotalSongsScript = () => {
 
     useEffect(() => {
         if (index >= skills.length) {
-            // Reset to the first skill when reaching the end
             setIndex(0);
         }
 
-        const skill = skills[index % skills.length]; // Use modulo to loop back to 0
+        const skill = skills[index % skills.length];
 
         if (subIndex === skill.length + 1 && !reverse) {
-            // Once the full skill is typed out, pause then start deleting
-            setTimeout(() => setReverse(true), 1000); // Pause at the end before deleting
+            setTimeout(() => setReverse(true), 1000);
             return;
         }
 
         if (subIndex === 0 && reverse) {
-            // Once the skill is fully deleted, move to the next skill
             setReverse(false);
-            setIndex((prevIndex) => (prevIndex + 1) % skills.length); // Loop back to the start
+            setIndex((prevIndex) => (prevIndex + 1) % skills.length);
             return;
         }
 
-        // Handle the typing effect
         const timeout = setTimeout(() => {
             setCurrentSkill(skill.substring(0, subIndex));
             setSubIndex((prevSubIndex) => prevSubIndex + (reverse ? -1 : 1));
-        }, reverse ? 60 : 100); // Speed of typing or deleting
+        }, reverse ? 60 : 100);
 
         return () => clearTimeout(timeout);
     }, [subIndex, index, reverse, skills]);
